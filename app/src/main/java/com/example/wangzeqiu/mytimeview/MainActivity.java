70093ee39f825c426cd88e9f3b465de157b35431
zeqiu.wang@aiwinn.com <zeqiu.wang@aiwinn.com>
2017-10-09 09:23:15 +0800

    test

--- app/src/main/java/com/example/wangzeqiu/mytimeview/MainActivity.java
+++ app/src/main/java/com/example/wangzeqiu/mytimeview/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button5).setOnClickListener(this);
         findViewById(R.id.button6).setOnClickListener(this);
         //test1
+        //test2
+        //test2
     }
 
 
